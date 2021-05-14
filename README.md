7bc6bc9884fc4e5416878e7eec03a167a6a2a308
Chigozie Onyenanu <Emmanuel@Chigozies-Air.lan>
2021-05-14 04:42:20 +0100

    adding files for github pages

---
+++ README.md
@@ -0,0 +1,17 @@
+# About 
+This is a simple post-it note built using [React!](https://reactjs.org/docs/create-a-new-react-app.html) and [React Hooks!](https://reactjs.org/docs/hooks-intro.html).
+
+I have integrated *localstorage* for storing notes in your browser.
+
+## Run the code
+1. yarn add
+2. yarn start 
+
+## Enjoy Web app
+
+
+
+
+
+
+
---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import "./App.css";
+import NoteList from "./components/NoteList/NoteList.component";
+import SearchNote from "./components/SearchNote/SearchNote.component";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const [searchText, setsearchText] = useState("");
+  const [toggleMode, SettoggleMode] = useState(false);
+
+  const handleAddNote = (text) => {
+    const date = new Date();
+
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+
+    setNotes((prevState) => [...prevState, newNote]);
+  };
+
+  const deleteNote = (id) => {
+    const newDeleteNote = notes.filter((note) => note.id !== id);
+    setNotes(newDeleteNote);
+  };
+
+  const handleSearch = (text) => {
+    setsearchText(text);
+  };
+
+  useEffect(
+    () => {
+      const savedNotes = JSON.parse(localStorage.getItem('post-it-note'));
+      
+      if (savedNotes){
+        setNotes(savedNotes);
+      }
+    }
+    , []
+  )
+  useEffect(
+    () => {
+      localStorage.setItem('post-it-note', JSON.stringify(notes))
+    }
+    , [notes]
+  )
+
+  return (
+    <div className={`${toggleMode && "darkmode"}`}>
+      <div className="App">
+        <div className="header">
+          <h1>Just Do it!</h1>
+          <button
+            className="save"
+            onClick={() => SettoggleMode(!toggleMode)}
+            size="1.8em"
+          >
+            Toggle Mode
+          </button>
+        </div>
+        <SearchNote handleSearch={handleSearch} />
+        <NoteList
+          notes={notes.filter(({ text }) =>
+            text.toLowerCase().includes(searchText)
+          )}
+          handleAddNote={handleAddNote}
+          deleteNote={deleteNote}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
